8b960dcf1b4f26ade8fd0476b326d26bf91caa5f
Adrian J. De Niz <ajdeniz@gmail.com>
2018-05-16 01:48:36 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,16 +4,18 @@ function navToggle() {
   var navlinks = document.getElementById("navlinks");
   var biobtn = document.getElementById("z");
 
-  if(i == 0) {
-    // navlinks.setAttribute("style", "display: flex;");
+if(i == 0) {
     navlinks.style.visibility = "visible";
+    if(biobtn) {
     biobtn.style.visibility = "hidden";
+  }
     i++;
   }
   else {
-    // navlinks.removeAttribute("style", "display: none;");
     navlinks.style.visibility= "hidden";
-    biobtn.style.visibility = "visible";
+    if(biobtn) {
+      biobtn.style.visibility = "visible";
+    }
     i--;
   }
 }
